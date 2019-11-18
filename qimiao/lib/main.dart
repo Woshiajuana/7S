652f09c0271d445cc595902cec239503a1902d89
Woshiajuana <zhigang.chen@owulia.com>
2019-11-18 10:08:15 +0800

    subcode

--- qimiao/lib/main.dart
+++ qimiao/lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'WoosaiMall',
         theme: ThemeData(
-          primarySwatch: Colors.red,
+//          primarySwatch: MaterialColor(),
         ),
         routes: Application.router.routes,
       ),
