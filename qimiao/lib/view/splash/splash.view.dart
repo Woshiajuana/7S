ceb8f3611828dfc13c2ac8a8386c7d0a571ad9c5
Woshiajuana <zhigang.chen@owulia.com>
2019-11-18 15:08:21 +0800

    subcode

---
+++ qimiao/lib/view/splash/splash.view.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
