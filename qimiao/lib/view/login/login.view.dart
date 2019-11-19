dc7412603f03e1e55b4c3fe046298f2757cae22b
Woshiajuana <zhigang.chen@owulia.com>
2019-11-19 11:49:17 +0800

    subcode splash login

--- qimiao/lib/view/login/login.view.dart
+++ qimiao/lib/view/login/login.view.dart
@@ -1,84 +1,136 @@
-/*
- * Created by 李卓原 on 2018/10/13.
- * email: zhuoyuan93@gmail.com
- * 写一个贼特么好看的登录页面
- */
 
 import 'package:flutter/material.dart';
-import 'package:groovin_material_icons/groovin_material_icons.dart';
+import 'package:qimiao/common/application.dart';
 
 class LoginView extends StatefulWidget {
+
   @override
-  _LoginViewState createState() => _LoginViewState();
+  _LoginViewState createState() => new _LoginViewState();
 }
 
 class _LoginViewState extends State<LoginView> {
-  final _formKey = GlobalKey<FormState>();
-  String _email, _password;
-  bool _isObscure = true;
-  Color _eyeColor;
-  List _loginMethod = [
-    {
-      "title": "facebook",
-      "icon": GroovinMaterialIcons.facebook,
-    },
-    {
-      "title": "google",
-      "icon": GroovinMaterialIcons.google,
-    },
-    {
-      "title": "twitter",
-      "icon": GroovinMaterialIcons.twitter,
-    },
-  ];
+
+  String _username = '13127590698';
+  String _password = '111111';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Form(
-            key: _formKey,
-            child: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 22.0),
-              children: <Widget>[
-                SizedBox(
-                  height: kToolbarHeight,
-                ),
-                buildTitle(),
-                buildTitleLine(),
-                SizedBox(height: 70.0),
-                buildEmailTextField(),
-                SizedBox(height: 30.0),
-                buildPasswordTextField(context),
-                buildForgetPasswordText(context),
-                SizedBox(height: 60.0),
-                buildLoginButton(context),
-                SizedBox(height: 30.0),
-                buildOtherLoginText(),
-                buildOtherMethod(context),
-                buildRegisterText(context),
-              ],
-            )));
+    // TODO: implement build
+    return new Scaffold(
+      body: new ListView(
+        children: <Widget>[
+          _widgetHeaderSection(),
+          _widgetInputSection(),
+          _widgetForgetSection(),
+          _widgetButtonSection(),
+          _widgetRegisterLinkSection(),
+        ],
+      ),
+    );
   }
 
-  Align buildRegisterText(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Padding(
-        padding: EdgeInsets.only(top: 10.0),
-        child: Row(
+  // 头部 LOGO
+  Widget _widgetHeaderSection () {
+    return new Container(
+      child: new Image.asset(
+        Application.util.getImgPath('logo-300'),
+        width: 120.0,
+        height: 120.0,
+      ),
+    );
+  }
+
+  // 输入
+  Widget _widgetInputSection () {
+    String _strEmail;
+    String _strPassword;
+    bool _isObscure = false;
+    return new Container(
+      padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+      child: new Column(
+        children: <Widget>[
+          new TextFormField(
+            decoration: new InputDecoration(
+              labelText: '邮箱',
+            ),
+            validator: (String value) { return value; },
+            onSaved: (String value) => _strEmail = value,
+          ),
+          new SizedBox(height: 20.0),
+          new TextFormField(
+            obscureText: _isObscure,
+            decoration: new InputDecoration(
+              labelText: '密码',
+            ),
+            validator: (String value) { return value; },
+            onSaved: (String value) => _strPassword = value,
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 忘记密码
+  Widget _widgetForgetSection () {
+    return new Container(
+      padding: const EdgeInsets.only(right: 30.0, top: 15.0),
+      child: new Align(
+        alignment: Alignment.centerRight,
+        child: new InkWell(
+          onTap: () => {},
+          child: new Text(
+            '忘记密码？',
+            style: new TextStyle(
+              fontSize: 14.0,
+              color: Color(0xff333333),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 注册
+  Widget _widgetRegisterLinkSection () {
+    return new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(
+            '没有账号？ ',
+          ),
+          new InkWell(
+            onTap: () => {},
+            child: new Text(
+              '点击注册',
+              style: TextStyle(color: Colors.green),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 登录
+  Widget _widgetButtonSection () {
+    return new Container(
+      height: 45.0,
+      margin: const EdgeInsets.only(left: 60.0, right: 60.0, top: 60.0),
+      decoration: new BoxDecoration(
+        color: Colors.blue,
+        borderRadius: new BorderRadius.circular(30.0),
+      ),
+      child: new FlatButton(
+        onPressed: () => {},
+        child: new Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('没有账号？'),
-            GestureDetector(
-              child: Text(
-                '点击注册',
-                style: TextStyle(color: Colors.green),
+            new Text(
+              '登录',
+              style: new TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
               ),
-              onTap: () {
-                //TODO 跳转到注册页面
-                print('去注册');
-                Navigator.pop(context);
-              },
             ),
           ],
         ),
@@ -86,147 +138,8 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 
-  ButtonBar buildOtherMethod(BuildContext context) {
-    return ButtonBar(
-      alignment: MainAxisAlignment.center,
-      children: _loginMethod
-          .map((item) => Builder(
-        builder: (context) {
-          return IconButton(
-              icon: Icon(item['icon'],
-                  color: Theme.of(context).iconTheme.color),
-              onPressed: () {
-                //TODO : 第三方登录方法
-                Scaffold.of(context).showSnackBar(new SnackBar(
-                  content: new Text("${item['title']}登录"),
-                  action: new SnackBarAction(
-                    label: "取消",
-                    onPressed: () {},
-                  ),
-                ));
-              });
-        },
-      ))
-          .toList(),
-    );
-  }
+  // 提交
+  void _handleSubmit() async {
 
-  Align buildOtherLoginText() {
-    return Align(
-        alignment: Alignment.center,
-        child: Text(
-          '其他账号登录',
-          style: TextStyle(color: Colors.grey, fontSize: 14.0),
-        ));
-  }
-
-  Align buildLoginButton(BuildContext context) {
-    return Align(
-      child: SizedBox(
-        height: 45.0,
-        width: 270.0,
-        child: RaisedButton(
-          child: Text(
-            'Login',
-            style: Theme.of(context).primaryTextTheme.headline,
-          ),
-          color: Colors.black,
-          onPressed: () {
-            if (_formKey.currentState.validate()) {
-              ///只有输入的内容符合要求通过才会到达此处
-              _formKey.currentState.save();
-              //TODO 执行登录方法
-              print('email:$_email , assword:$_password');
-            }
-          },
-          shape: StadiumBorder(side: BorderSide()),
-        ),
-      ),
-    );
-  }
-
-  Padding buildForgetPasswordText(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0),
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: FlatButton(
-          child: Text(
-            '忘记密码？',
-            style: TextStyle(fontSize: 14.0, color: Colors.grey),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-
-  TextFormField buildPasswordTextField(BuildContext context) {
-    return TextFormField(
-      onSaved: (String value) => _password = value,
-      obscureText: _isObscure,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return '请输入密码';
-        }
-      },
-      decoration: InputDecoration(
-          labelText: 'Password',
-          suffixIcon: IconButton(
-              icon: Icon(
-                Icons.remove_red_eye,
-                color: _eyeColor,
-              ),
-              onPressed: () {
-                setState(() {
-                  _isObscure = !_isObscure;
-                  _eyeColor = _isObscure
-                      ? Colors.grey
-                      : Theme.of(context).iconTheme.color;
-                });
-              })),
-    );
-  }
-
-  TextFormField buildEmailTextField() {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: 'Emall Address',
-      ),
-      validator: (String value) {
-        var emailReg = RegExp(
-            r"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
-        if (!emailReg.hasMatch(value)) {
-          return '请输入正确的邮箱地址';
-        }
-      },
-      onSaved: (String value) => _email = value,
-    );
-  }
-
-  Padding buildTitleLine() {
-    return Padding(
-      padding: EdgeInsets.only(left: 12.0, top: 4.0),
-      child: Align(
-        alignment: Alignment.bottomLeft,
-        child: Container(
-          color: Colors.black,
-          width: 40.0,
-          height: 2.0,
-        ),
-      ),
-    );
-  }
-
-  Padding buildTitle() {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Login',
-        style: TextStyle(fontSize: 42.0),
-      ),
-    );
   }
 }
