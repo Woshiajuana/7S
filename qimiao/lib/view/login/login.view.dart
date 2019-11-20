60726d04a014a5f9949514b1bd9dc19b888d5cff
Woshiajuana <zhigang.chen@owulia.com>
2019-11-20 18:49:14 +0800

    subcode

--- qimiao/lib/view/login/login.view.dart
+++ qimiao/lib/view/login/login.view.dart
@@ -10,8 +10,8 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
 
-  String _username = '13127590698';
-  String _password = '111111';
+  String _strEmail;
+  String _strPassword;
 
   @override
   Widget build(BuildContext context) {
@@ -67,15 +67,57 @@ class _LoginViewState extends State<LoginView> {
 
   // 输入框
   Widget _widgetInputSection () {
-    return new Container(
-
+    return new Center(
+      child: new Container(
+        width: 260.0,
+        height: 46.0,
+        decoration: new BoxDecoration(
+          color: Color(0xffe4e3e0),
+          borderRadius: new BorderRadius.circular(30.0),
+        ),
+        child: new Row(
+          children: <Widget>[
+            new Container(
+              alignment: Alignment.center,
+              width: 46.0,
+              height: 46.0,
+              child: new Image.asset(Application.util.getImgPath('user_icon.png'), width: 16.0, height: 16.0),
+            ),
+            new Expanded(
+              flex: 1,
+              child: new TextFormField(
+                decoration: new InputDecoration(
+                  hintText: '请输入用户名',
+                  disabledBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+                onSaved: (String value) => _strEmail = value,
+              ),
+            ),
+            new Offstage(
+              offstage: false,
+              child: new IconButton(
+                icon: new Icon(Icons.clear, size: 20.0, color: Color(0xff666666)),
+                onPressed: () => print(1),
+              ),
+            ),
+            new Offstage(
+              offstage: false,
+              child: new IconButton(
+                icon: new Icon(Icons.remove_red_eye, size: 20.0, color: Color(0xff666666)),
+                onPressed: () => print(1),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   // 输入
   Widget _widgetInputSection1 () {
-    String _strEmail;
-    String _strPassword;
+
     bool _isObscure = false;
     return new Container(
       padding: const EdgeInsets.only(left: 30.0, right: 30.0),
