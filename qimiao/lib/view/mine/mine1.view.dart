de727c23319cf040ad0bd719e3e1b375e018aee3
Woshiajuana <zhigang.chen@owulia.com>
2019-11-22 09:50:38 +0800

    subcode

--- qimiao/lib/view/mine/mine1.view.dart
+++ qimiao/lib/view/mine/mine1.view.dart
@@ -13,43 +13,39 @@ class _MineViewState extends State<MineView> {
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Application.config.style.backgroundColor,
-      body: new CustomScrollView(
-        slivers: <Widget>[
-          new SliverAppBar(
-            title: new Text(
-              'SliverAppBarParams',
-              style: new TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            // 展开的高度
-            expandedHeight: 300.0,
-            // 强制显示阴影
-            forceElevated: true,
-            // 设置该属性，当有下滑手势的时候，就会显示 AppBar
-            floating: true,
-            // 该属性只有在 floating 为 true 的情况下使用，不然会报错
-            // 当下滑到一定比例，会自动把 AppBar 展开
-//            snap: true,
-            // 设置该属性使 Appbar 折叠后不消失
-            pinned: true,
-            // 通过这个属性设置 AppBar 的背景
-            flexibleSpace: new FlexibleSpaceBar(
-              // 背景折叠动画
-              collapseMode: CollapseMode.parallax,
-              background: Image.network(Application.config.style.srcDemoUrl, fit: BoxFit.cover),
-            ),
-          ),
-          // 这个部件一般用于最后填充用的，会占有一个屏幕的高度，
-          // 可以在 child 属性加入需要展示的部件
-          new SliverFillRemaining(
-            child: new Center(
-                child: new Text(
-                  'content',
-                  style: new TextStyle(fontSize: 30.0),
-                )
+      appBar: new AppBar(
+        elevation: 0,
+        actions: <Widget>[
+          new Container(
+            child: new Stack(
+              children: <Widget>[
+                new IconButton(
+                  icon: new Icon(Icons.email),
+                  onPressed: () => Application.router.push(context, 'notice'),
+                ),
+                new Positioned(
+                  top: 10.0,
+                  right: 10.0,
+                  child: new Container(
+                    width: 10.0,
+                    height: 10.0,
+                    decoration: new BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: new BorderRadius.circular(6.0),
+                      border: new Border.all(color: Application.config.style.mainColor, width: 2.0),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+        ],
+      ),
+      body: new ListView(
+        children: <Widget>[
+          _widgetHeaderSection(),
+          _widgetFollowSection(),
+          _widgetMenuSection(),
         ],
       ),
     );
