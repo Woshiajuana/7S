6296ab361519fde9159620b510b0fe23277a7deb
Woshiajuana <zhigang.chen@owulia.com>
2019-11-22 16:52:28 +0800

    subcode

--- qimiao/lib/view/mine/mine.view.dart
+++ qimiao/lib/view/mine/mine.view.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:qimiao/common/application.dart';
-import 'package:flutter/services.dart';
 
 class MineView extends StatefulWidget {
   @override
@@ -14,262 +13,85 @@ class _MineViewState extends State<MineView> {
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Application.config.style.backgroundColor,
-      body: new CustomScrollView(
-        slivers: <Widget>[
-          new SliverPersistentHeader(
-            pinned: true,
-            delegate: SliverCustomHeaderDelegate(
-                title: '我的',
-                collapsedHeight: 56,
-                expandedHeight: 300,
-                paddingTop: MediaQuery.of(context).padding.top,
-                coverImgUrl: 'https://img.zcool.cn/community/01c6615d3ae047a8012187f447cfef.jpg@1280w_1l_2o_100sh.jpg'
-            ),
-          ),
-          new SliverList(
-            delegate: new SliverChildListDelegate(
-              <Widget>[
-                _widgetMenuSection(),
-                _widgetMenuSection(),
-                _widgetMenuSection(),
-                _widgetMenuSection(),
-              ]
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // 粉丝 or 关注
-  Widget _widgetFollowSection () {
-    return new Container(
-      color: Application.config.style.mainColor,
-      height: 70.0,
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: new ListView(
+        padding: const EdgeInsets.only(top: 0),
         children: <Widget>[
-          new Expanded(
-            flex: 1,
-            child: new FlatButton(
-                onPressed: () => {},
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new Text(
-                      '1000',
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    new SizedBox(height: 3.0),
-                    new Text(
-                      '关注',
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 13.0,
-                      ),
-                    ),
-                  ],
-                )
+          new Container(
+            height: 310,
+            child: new Stack(
+              children: <Widget>[
+                // 头部背景
+                _widgetHeaderBgSection(),
+                // 头部 appbar
+                _widgetAppBarSection(),
+                // 头部内容
+                _widgetHeaderSection(),
+              ],
             ),
           ),
-          new Expanded(
-            flex: 1,
-            child: new FlatButton(
-                onPressed: () => {},
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new Text(
-                      '1000',
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    new SizedBox(height: 3.0),
-                    new Text(
-                      '粉丝',
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 13.0,
-                      ),
-                    ),
-                  ],
-                )
-            ),
-          ),
+          // 菜单内容
+          _widgetMenuSection(),
         ],
       ),
     );
   }
 
-  // 菜单
-  Widget _widgetMenuSection () {
-    List _arrMenu = [
-      {
-        'text': '视频',
-        'icon': Icons.videocam,
-        'color': Color(0xff43bdbe),
-        'routeName': 'setting',
-      },
-      {
-        'text': '相册',
-        'icon': Icons.photo_camera,
-        'color': Color(0xffd76c93),
-        'routeName': 'setting',
-      },
-      {
-        'text': '收藏',
-        'icon': Icons.headset,
-        'color': Color(0xff7c4a7d),
-        'routeName': 'setting',
-      },
-      {
-        'text': '设置',
-        'icon': Icons.settings,
-        'color': Color(0xffeacb5f),
-        'routeName': 'setting',
-      },
-    ];
-
-    Widget _widgetMenuItem ({
-      dynamic onPressed,
-      Color color,
-      String text = '',
-      IconData icon,
-    }) {
-      return new Container(
-        height: 77.0,
-        color: Colors.white,
-        child: new FlatButton(
-          padding: const EdgeInsets.all(0),
-          onPressed: onPressed,
-          child: new Row(
+  // appbar
+  Widget _widgetAppBarSection () {
+    return new AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      actions: <Widget>[
+        new Container(
+          child: new Stack(
             children: <Widget>[
-              new Container(
-                width: 77.0,
-                height: 77.0,
-                color: color,
-                child: new Icon(icon, color: Colors.white),
+              new IconButton(
+                icon: new Icon(Icons.email),
+                onPressed: () => Application.router.push(context, 'notice'),
               ),
-              new Expanded(
-                flex: 1,
+              new Positioned(
+                top: 10.0,
+                right: 10.0,
                 child: new Container(
+                  width: 10.0,
+                  height: 10.0,
                   decoration: new BoxDecoration(
-                    border: new Border(
-                        bottom: new BorderSide(width: 0.5, color: Color(0xffdddddd))
-                    ),
-                  ),
-                  child: new Row(
-                    children: <Widget>[
-                      new SizedBox(width: 16.0),
-                      new Text(
-                        text,
-                        style: new TextStyle(
-                          color: Color(0xff333333),
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      new Expanded(flex: 1, child: new Container()),
-                      new Icon(Icons.arrow_forward_ios, size: 18.0, color: Color(0xff999999)),
-                      new SizedBox(width: 10.0),
-                    ],
+                    color: Colors.red,
+                    borderRadius: new BorderRadius.circular(6.0),
+                    border: new Border.all(color: Application.config.style.mainColor, width: 2.0),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
-      );
-    }
-
-    return new Column(
-      children: _arrMenu.map((item) {
-        return _widgetMenuItem(
-          onPressed: () => Application.router.push(context, item['routeName']),
-          text: item['text'],
-          icon: item['icon'],
-          color: item['color'],
-        );
-      }).toList(),
+      ],
     );
   }
 
-}
-
-class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
-  final double collapsedHeight;
-  final double expandedHeight;
-  final double paddingTop;
-  final String coverImgUrl;
-  final String title;
-
-  SliverCustomHeaderDelegate({
-    this.collapsedHeight,
-    this.expandedHeight,
-    this.paddingTop,
-    this.coverImgUrl,
-    this.title,
-  });
-
-  @override
-  double get minExtent => this.collapsedHeight + this.paddingTop;
-
-  @override
-  double get maxExtent => this.expandedHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
-  }
-
-  void updateStatusBarBrightness(shrinkOffset) {
-    if(shrinkOffset > 50) {
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light,
-      ));
-    } else if(shrinkOffset <= 50) {
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.dark,
-      ));
-    }
-  }
-
-  Color makeStickyHeaderBgColor(shrinkOffset) {
-    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-    return Color.fromARGB(alpha, 236, 100, 47);
-  }
-
-  Color makeStickyHeaderTextColor(shrinkOffset, isIcon) {
-    if(shrinkOffset <= 50) {
-      return isIcon ? Colors.white : Colors.transparent;
-    } else {
-      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-      return Color.fromARGB(alpha, 255, 255, 255);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    this.updateStatusBarBrightness(shrinkOffset);
+  // 头部背景
+  Widget _widgetHeaderBgSection () {
     return new Container(
-      height: this.maxExtent,
-      width: MediaQuery.of(context).size.width,
+      height: 310.0,
+      alignment: Alignment.bottomCenter,
+      decoration: new BoxDecoration(
+        color: Colors.blue,
+        image: new DecorationImage(
+          image: new AssetImage(Application.util.getImgPath('mine_head_bg.png')),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: new Stack(
-        fit: StackFit.expand,
         children: <Widget>[
-          new Container(child: new Image.network(this.coverImgUrl, fit: BoxFit.cover)),
+          new Container(
+            color: Color.fromRGBO(0, 0, 0, 0.2),
+          ),
           new Positioned(
-            left: 0,
-            top: this.maxExtent / 2,
-            right: 0,
             bottom: 0,
+            left: 0,
+            right: 0,
             child: new Container(
+              height: 100.0,
               decoration: new BoxDecoration(
                 gradient: new LinearGradient(
                   begin: Alignment.topCenter,
@@ -282,59 +104,216 @@ class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
               ),
             ),
           ),
-          new Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            child: new Container(
-              color: this.makeStickyHeaderBgColor(shrinkOffset),
-              child: new SafeArea(
-                bottom: false,
-                child: new Container(
-                  height: this.collapsedHeight,
-                  child: new Row(
-                    children: <Widget>[
-                      new SizedBox(width: 16.0),
-                      new Text(
-                        this.title,
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w500,
-                          color: this.makeStickyHeaderTextColor(shrinkOffset, false),
-                        ),
-                      ),
-                      new Expanded(child: new Container(), flex: 1),
-                      new Container(
-                        child: new Stack(
-                          children: <Widget>[
-                            new IconButton(
-                              icon: new Icon(Icons.email, color: Colors.white,),
-                              onPressed: () => Application.router.push(context, 'notice'),
-                            ),
-                            new Positioned(
-                              top: 10.0,
-                              right: 10.0,
-                              child: new Container(
-                                width: 10.0,
-                                height: 10.0,
-                                decoration: new BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: new BorderRadius.circular(6.0),
-                                  border: new Border.all(color: this.makeStickyHeaderBgColor(shrinkOffset), width: 2.0),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+        ],
+      ),
+    );
+  }
+
+  // 头部内容
+  Widget _widgetHeaderSection () {
+    return new Container(
+      height: 310,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          _widgetAvatarSection(),
+          _widgetFollowGroup(),
+        ],
+      ),
+    );
+  }
+
+  Widget _widgetAvatarSection () {
+    return new Container(
+      padding: const EdgeInsets.only(bottom: 20.0, top: 20.0),
+      child: new Column(
+        children: <Widget>[
+          // 头像
+          new Container(
+            decoration: new BoxDecoration(
+              border: new Border.all(color: Colors.white, width: 2.0), // 边色与边宽度
+              color: Color(0xFF9E9E9E), // 底色
+              borderRadius: new BorderRadius.circular((41)), // 圆角度
+            ),
+            child: new ClipOval(
+              child: new FadeInImage.assetNetwork(
+                width: 80.0,
+                height: 80.0,
+                placeholder: Application.config.style.srcGoodsNull,
+                image: 'http://ossmk2.jfpays.com/www_make_v1/app/static/images/defaultFace013x.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          new SizedBox(height: 5.0),
+          // 昵称
+          new Container(
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  '我是阿倦啊',
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
                   ),
                 ),
-              ),
+              ],
             ),
           ),
         ],
       ),
     );
   }
+
+  // 粉丝 or 关注
+  Widget _widgetFollowGroup () {
+    Widget _widgetBaseInfoItem ({
+      String labelText = '',
+      String valueText = '',
+    }) {
+      return new Expanded(
+        flex: 1,
+        child: new FlatButton(
+            onPressed: () => {},
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  valueText,
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                new SizedBox(height: 3.0),
+                new Text(
+                  labelText,
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+              ],
+            )
+        ),
+      );
+    }
+    return new Container(
+      height: 70.0,
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          _widgetBaseInfoItem(
+            labelText: '粉丝',
+            valueText: '1240',
+          ),
+          _widgetBaseInfoItem(
+            labelText: '关注',
+            valueText: '228',
+          ),
+          _widgetBaseInfoItem(
+            labelText: '视频',
+            valueText: '228',
+          ),
+          _widgetBaseInfoItem(
+            labelText: '作品',
+            valueText: '55',
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 菜单
+  Widget _widgetMenuSection () {
+    List _arrMenu = [
+      {
+        'text': '视频',
+        'icon': Icons.videocam,
+        'useMargin': true,
+        'routeName': 'setting',
+      },
+      {
+        'text': '相册',
+        'icon': Icons.photo_camera,
+        'useMargin': false,
+        'routeName': 'setting',
+      },
+      {
+        'text': '点赞',
+        'icon': Icons.headset,
+        'useMargin': true,
+        'routeName': 'setting',
+      },
+      {
+        'text': '收藏',
+        'icon': Icons.headset,
+        'useMargin': false,
+        'routeName': 'setting',
+      },
+      {
+        'text': '设置',
+        'icon': Icons.settings,
+        'useMargin': true,
+        'routeName': 'setting',
+      },
+    ];
+
+    Widget _widgetMenuItem ({
+      dynamic onPressed,
+      Color color,
+      String text = '',
+      IconData icon,
+      bool useMargin = false,
+    }) {
+      return new Container(
+        height: 60.0,
+        margin: EdgeInsets.only(top: useMargin ? 10.0 : 0),
+        decoration: new BoxDecoration(
+          color: Colors.white,
+          border: new Border(
+              bottom: new BorderSide(width: 0.5, color: Color(0xffdddddd)),
+              top: new BorderSide(width: useMargin ? 0.5 : 0, color: Color(0xffdddddd))
+          ),
+        ),
+        child: new FlatButton(
+          padding: const EdgeInsets.all(0),
+          onPressed: onPressed,
+          child: new Row(
+            children: <Widget>[
+              new SizedBox(width: 16.0),
+              new Icon(icon, color: Color(0xff333333)),
+              new SizedBox(width: 16.0),
+              new Text(
+                text,
+                style: new TextStyle(
+                  color: Color(0xff333333),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              new Expanded(flex: 1, child: new Container()),
+              new Icon(Icons.arrow_forward_ios, size: 18.0, color: Color(0xff999999)),
+              new SizedBox(width: 10.0),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return new Column(
+      children: _arrMenu.map((item) {
+        return _widgetMenuItem(
+          onPressed: () => Application.router.push(context, item['routeName']),
+          text: item['text'],
+          icon: item['icon'],
+          useMargin: item['useMargin'],
+        );
+      }).toList(),
+    );
+  }
+
 }
