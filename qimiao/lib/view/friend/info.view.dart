aa70c33cdcf041d74eb03ff5998ea262fca9fc76
Woshiajuana <zhigang.chen@owulia.com>
2019-11-26 13:52:38 +0800

    subcode info widget

--- qimiao/lib/view/friend/info.view.dart
+++ qimiao/lib/view/friend/info.view.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:qimiao/common/application.dart';
-import 'package:flutter/services.dart';
+import 'package:qimiao/widget/widget.dart';
 
 class FriendInfoView extends StatefulWidget {
   @override
@@ -411,83 +411,5 @@ class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
     return true;
-  }
-}
-
-class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
-  final double collapsedHeight;
-  final double expandedHeight;
-  final double paddingTop;
-  final buildContent;
-  SliverCustomHeaderDelegate({
-    this.collapsedHeight,
-    this.expandedHeight,
-    this.paddingTop,
-    this.buildContent,
-  });
-
-  @override
-  double get minExtent => this.collapsedHeight + this.paddingTop;
-
-  @override
-  double get maxExtent => this.expandedHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
-  }
-
-  void updateStatusBarBrightness(shrinkOffset) {
-    if(shrinkOffset > 50) {
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light,
-      ));
-    } else if(shrinkOffset <= 50) {
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.dark,
-      ));
-    }
-  }
-
-  Color makeStickyHeaderBgColor(shrinkOffset) {
-    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-    return Color.fromARGB(alpha, 236, 100, 47);
-  }
-
-  Color makeStickyHeaderTextColor(shrinkOffset, isIcon) {
-    if(shrinkOffset <= 50) {
-      return isIcon ? Colors.white : Colors.transparent;
-    } else {
-      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-      return Color.fromARGB(alpha, 255, 255, 255);
-    }
-  }
-
-  Color makeStickyContentTextColor(shrinkOffset, isIcon) {
-    if(shrinkOffset <= 50) {
-      return isIcon ? Colors.white : Colors.transparent;
-    } else {
-      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-      return Color.fromARGB(1-alpha, 255, 255, 255);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    this.updateStatusBarBrightness(shrinkOffset);
-    return new Container(
-      height: this.maxExtent,
-      width: MediaQuery.of(context).size.width,
-      child: new Stack(
-        fit: StackFit.expand,
-        children: this.buildContent(
-          context,
-          shrinkOffset,
-          (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt(),
-        ),
-      ),
-    );
   }
 }
--- qimiao/lib/widget/widget.dart
+++ qimiao/lib/widget/widget.dart
@@ -4,3 +4,4 @@ library widget;
 export 'confirmDialog.widget.dart';
 export 'upgradeDialog.widget.dart';
 export 'actionSheetDialog.widget.dart';
+export 'sliverCustomHeaderDelegate.widget.dart';
