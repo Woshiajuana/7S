7aebd16036b44bc0ffe4fbeeee1036cdd5cd1dce
Woshiajuana <zhigang.chen@owulia.com>
2019-11-26 16:26:53 +0800

    subcode

--- qimiao/lib/view/world/world.view.dart
+++ qimiao/lib/view/world/world.view.dart
@@ -1,134 +1,73 @@
 
 import 'package:flutter/material.dart';
 import 'package:qimiao/common/application.dart';
-import 'package:qimiao/widget/cellLink.widget.dart';
 import 'package:qimiao/widget/widget.dart';
 import 'package:flukit/flukit.dart';
+
+import 'package:flutter/widgets.dart';
 
 class WorldView extends StatefulWidget {
   @override
   _WorldViewState createState() => _WorldViewState();
 }
 
-class _WorldViewState extends State<WorldView> {
+class _WorldViewState extends State<WorldView> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+  final List _tabBarList = [
+    {"id": "5b1fdbee30025ae5371ac363", "name": "动漫"},
+    {"id": "5b1fd85730025ae5371abaed", "name": "综艺"}
+  ];
+
+//  @override
+//  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Application.config.style.backgroundColor,
-      body: new CustomScrollView(
-        slivers: <Widget>[
-          new SliverPersistentHeader(
+    return NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            floating: true,
             pinned: true,
-            delegate: new SliverCustomHeaderDelegate(
-              collapsedHeight: 56,
-              expandedHeight: 200,
-              paddingTop: MediaQuery.of(context).padding.top,
-              buildContent: (BuildContext context, double shrinkOffset, int alpha) {
-                return <Widget> [
-                  _widgetCarouselSection(shrinkOffset: shrinkOffset, alpha: alpha),
-                  _widgetAppBarSection(shrinkOffset: shrinkOffset, alpha: alpha),
-                ];
-              }
-            ),
-          ),
-          new SliverList(
-            delegate: new SliverChildListDelegate(
-              <Widget>[
-                new Container(height: 200.0, color: Colors.blue),
-                new Container(height: 200.0, color: Colors.red),
-                new Container(height: 200.0, color: Colors.blue),
-                new Container(height: 200.0, color: Colors.red),
-                new Container(height: 200.0, color: Colors.blue),
-                new Container(height: 200.0, color: Colors.red),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // 轮播图
-  Widget _widgetCarouselSection ({
-    double shrinkOffset,
-    int alpha,
-  }) {
-
-    // 引导页数据
-    List<String> _arrGuide = [
-      Application.util.getImgPath('guide1.png'),
-      Application.util.getImgPath('guide2.png'),
-      Application.util.getImgPath('guide3.png'),
-      Application.util.getImgPath('guide4.png'),
-    ];
-
-    // 引导页轮播
-    return new Swiper(
-      autoStart: false,
-      circular: false,
-      indicator: new CircleSwiperIndicator(
-        radius: 4.0,
-        padding: const EdgeInsets.only(bottom: 40.0),
-        itemColor: Colors.black26,
-      ),
-      children: _arrGuide.map((item) {
-        return new Container(
-          child: new Image.asset(
-            item,
-            fit: BoxFit.fill,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-        );
-      }).toList(),
-    );
-
-  }
-
-  // 导航条
-  Widget _widgetAppBarSection ({
-    double shrinkOffset,
-    int alpha,
-  }) {
-
-    return new Positioned(
-      left: 0,
-      right: 0,
-      top: 0,
-      child: new Container(
-        color: Color.fromARGB(alpha, 236, 100, 47),
-        child: new SafeArea(
-          bottom: false,
-          child: new Container(
-            height: 56.0,
-            child: new Row(
-              children: <Widget>[
-                new IconButton(
-                  icon: new Icon(
-                    Icons.arrow_back,
-                    color: shrinkOffset <= 50 ? Colors.white : Color.fromARGB(alpha, 255, 255, 255),
-                  ),
-                  onPressed: () => Navigator.pop(context),
+            // snap: true,
+            centerTitle: true,
+            title: Text("Popular", style: TextStyle(fontFamily: "Lobster")),
+            backgroundColor: Theme.of(context).primaryColor,
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, 36.0),
+              child: Container(
+                height: 36.0,
+                child: TabBar(
+                  controller: _tabController,
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Color.fromARGB(255,192, 193, 195),
+                  indicator: TabBarIndictorComponent(context: context),
+                  tabs: <Widget>[
+                    Tab(text: "动漫"),
+                    Tab(text: "综艺")
+                  ],
                 ),
-                new SizedBox(width: 24.0),
-                new Text(
-                  '我是阿倦啊',
-                  style: new TextStyle(
-                    color: shrinkOffset <= 50 ? Colors.transparent : Color.fromARGB(alpha, 255, 255, 255),
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          )
+        ];
+      },
+      body: TabBarView(
+          controller: _tabController,
+          children: [
+            new Container(child: new Text('1'),),
+            new Container(child: new Text('2'),)
+          ]
       ),
     );
-
   }
-
 
 
   void _handleExitOut () async {
@@ -154,4 +93,38 @@ class _WorldViewState extends State<WorldView> {
   }
 
 
+}
+
+
+class TabBarIndictorComponent extends Decoration {
+
+  BuildContext context;
+  Color bgColor;
+
+  TabBarIndictorComponent({ @required this.context, this.bgColor = Colors.white });
+
+  @override
+  BoxPainter createBoxPainter([onChanged]) => _TabBarIndictorBoxPainter(context, bgColor);
+}
+
+class _TabBarIndictorBoxPainter extends BoxPainter {
+
+  BuildContext context;
+  Color bgColor;
+
+  _TabBarIndictorBoxPainter(this.context, this.bgColor);
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final Paint paint = Paint();
+    paint.color = bgColor;
+    paint.style = PaintingStyle.fill;
+    final width = 33.0;
+    final height = 2.0;
+    canvas.drawRect(
+        Rect.fromLTWH(offset.dx - width / 2 + configuration.size.width / 2,
+            configuration.size.height - height - 5.0, width, height),
+        paint
+    );
+  }
 }
