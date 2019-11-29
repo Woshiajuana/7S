2540fedcd1fe43c34e351c1a2a74a77e38df0de7
Woshiajuana <zhigang.chen@owulia.com>
2019-11-29 16:26:44 +0800

    subcode calendar

--- qimiao/lib/view/calendar/calendar.view.dart
+++ qimiao/lib/view/calendar/calendar.view.dart
@@ -27,38 +27,95 @@ class _CalendarViewState extends State<CalendarView> {
     return new Scaffold(
       backgroundColor: Application.config.style.backgroundColor,
       appBar: new AppBar(
-        centerTitle: true,
-        title: new Container(
-          child: new Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-//              new IconButton(
-//                icon: new Icon(Icons.arrow_back_ios, size: 18.0),
-//                onPressed: () => {},
-//              ),
-              new Text(
-                Utils.apiDayFormat(_dateTime),
-                style: new TextStyle(
-                  fontSize: 18.0,
-                ),
-              ),
-//              new IconButton(
-//                icon: new Icon(Icons.arrow_forward_ios, size: 18.0),
-//                onPressed: () => {},
-//              ),
-            ],
+        title: new Text(
+          '定格',
+          style: new TextStyle(
+            fontSize: 18.0,
           ),
         ),
         actions: <Widget>[
           new IconButton(
-            icon: new Icon(Icons.calendar_today, color: Colors.white),
+            icon: new Icon(Icons.help, color: Colors.white),
             onPressed: () => {},
           )
         ],
       ),
       body: new ListView(
         children: <Widget>[
+          // 日历
           _widgetCalendarSection(),
+          // 标题
+          _widgetTitleSection(),
+          // 视频
+          _widgetVideoSection(),
+          // 照片
+          _widgetPhotoSection(),
+        ],
+      ),
+    );
+  }
+
+
+  // 标题
+  Widget _widgetTitleSection () {
+    return new Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Text(
+            Utils.apiDayFormat(_dateTime),
+            style: new TextStyle(
+              fontSize: 18.0,
+              color: Color(0xff333333),
+            ),
+          ),
+          new SizedBox(height: 5.0),
+          new Text(
+            '您今日已拍摄一段视频，一张照片了哦...',
+            style: new TextStyle(
+              fontSize: 12.0,
+              color: Color(0xff999999),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 视频
+  Widget _widgetVideoSection () {
+    return new Container(
+      margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+      height: 160.0,
+      decoration: new BoxDecoration(
+        color: Colors.white,
+        borderRadius: new BorderRadius.all(new Radius.circular(6.0)),
+        boxShadow: [
+          new BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            offset: Offset(0.0, 1.0), //阴影xy轴偏移量
+          ),
+        ],
+      ),
+      child: new Stack(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+
+  // 照片
+  Widget _widgetPhotoSection () {
+    return new Container(
+      margin: const EdgeInsets.all(16.0),
+      color: Colors.red,
+      height: 160.0,
+      child: new Stack(
+        children: <Widget>[
+
         ],
       ),
     );
@@ -68,8 +125,16 @@ class _CalendarViewState extends State<CalendarView> {
   Widget _widgetCalendarSection () {
     int _millisecondsSinceEpoch = DateTime.now().millisecondsSinceEpoch;
     return new Container(
+      decoration: new BoxDecoration(
+        border: new Border(
+          bottom: new BorderSide(
+            color: Color(0xffdddddd),
+            width: 0.5,
+          ),
+        ),
+      ),
       child: new WowCalendar(
-        isExpandable: true,
+//        isExpandable: true,
         onSelectedRangeChange: (x) {
           print('onSelectedRangeChange=>$x');
         },
@@ -123,27 +188,6 @@ class _CalendarViewState extends State<CalendarView> {
     }
   }
 
-  Widget _widgetHeaderSection () {
-    return new Container(
-      margin: const EdgeInsets.only(top: 100.0, bottom: 50.0),
-      child: new Column(
-        children: <Widget>[
-          new Icon(
-            Icons.face,
-            size: 100,
-            color: Application.config.style.mainColor,
-          ),
-          new Text(
-            'WowFlutter v0.0.1',
-            style: new TextStyle(
-              color: Application.config.style.mainColor,
-              fontSize: 18.0,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 
 
 }
