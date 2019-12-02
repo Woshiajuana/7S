4a0bdd7ce0760538372e91f22c147cdd9389d7f2
Woshiajuana <zhigang.chen@owulia.com>
2019-12-02 10:24:40 +0800

    subcode details wowScrollerInfo

--- qimiao/lib/view/video/details.dart
+++ qimiao/lib/view/video/details.dart
@@ -9,25 +9,49 @@ class VideoDetailsView extends StatefulWidget {
 }
 
 class _VideoDetailsViewState extends State<VideoDetailsView> {
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Application.config.style.mainColor,
-      body: new NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            new SliverAppBar(
-              pinned: true,
-              
-            ),
-          ];
-        },
-        body: new ListView(
+      body: new NotificationListener<ScrollNotification>(
+        onNotification: _handleScroll,
+        child: new Stack(
           children: <Widget>[
-
+            new ListView(
+              children: <Widget>[
+                new Container(color: Colors.blue, height: 200.0),
+                new SizedBox(height: 10.0),
+                new Container(color: Colors.red, height: 200.0),
+                new SizedBox(height: 10.0),
+                new Container(color: Colors.red, height: 200.0),
+                new SizedBox(height: 10.0),
+                new Container(color: Colors.red, height: 200.0),
+                new SizedBox(height: 10.0),
+                new Container(color: Colors.red, height: 200.0),
+                new SizedBox(height: 10.0),
+                new Container(color: Colors.red, height: 200.0),
+                new SizedBox(height: 10.0),
+              ],
+            )
           ],
         ),
       ),
     );
   }
+
+  int _alpha = 255;
+
+  // 滚动监听回调
+  bool _handleScroll (ScrollNotification scroll) {
+    // 当前滑动距离
+    double currentExtent = scroll.metrics.pixels;
+    final int alpha = (currentExtent / 200 * 255).clamp(0, 255).toInt();
+
+    print('alpha=> $alpha');
+    // 返回false，继续向上传递,返回true则不再向上传递
+    return false;
+  }
+
 }
---
+++ qimiao/lib/widget/wowScrollerInfo.widget.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class WowScrollerInfo extends StatefulWidget {
+
+  final builder;
+
+  WowScrollerInfo({
+    this.builder,
+    
+  });
+
+  @override
+  _WowScrollerInfoState createState() => _WowScrollerInfoState();
+}
+
+class _WowScrollerInfoState extends State<WowScrollerInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
