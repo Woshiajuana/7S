0754e7072db0701c0ee33dc610905a63c4e4059e
Woshiajuana <zhigang.chen@owulia.com>
2019-12-02 09:57:10 +0800

    subcode video details

--- qimiao/lib/view/video/details.dart
+++ qimiao/lib/view/video/details.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:qimiao/common/application.dart';
+import 'package:qimiao/widget/widget.dart';
 
 class VideoDetailsView extends StatefulWidget {
   @override
@@ -11,7 +12,22 @@ class _VideoDetailsViewState extends State<VideoDetailsView> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Application.config.style.mainColor,
+      body: new NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            new SliverAppBar(
+              pinned: true,
+              
+            ),
+          ];
+        },
+        body: new ListView(
+          children: <Widget>[
 
+          ],
+        ),
+      ),
     );
   }
 }
