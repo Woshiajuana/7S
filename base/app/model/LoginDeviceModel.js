990c83158fccd362335384c73d5ba10f59f2dfd5
Woshiajuana <zhigang.chen@owulia.com>
2019-12-03 14:22:35 +0800

    subcode

---
+++ base/app/model/LoginDeviceModel.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+module.exports = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+    const postSchema = new Schema({
+
+        // 用户
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            trim: true,
+            required: true,
+        },
+
+        // 设备
+        device: {
+            type: String,
+            trim: '',
+            default: '',
+        },
+
+        // ip
+        ip: {
+            
+        },
+
+        // 创建时间
+        created_at: {
+            type: Date,
+            default: Date.now,
+        },
+
+        // 更新时间
+        updated_at: {
+            type: Date,
+            default: Date.now,
+        },
+
+    });
+    return mongoose.model('login_device', postSchema);
+};
