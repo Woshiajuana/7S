3604a00d4f598bab2a957e011619be6ff9fb18ac
Woshiajuana <zhigang.chen@owulia.com>
2019-12-04 15:31:25 +0800

    subcode

--- gateway/front-end/app/controller/UserController.js
+++ gateway/front-end/app/controller/UserController.js
@@ -32,10 +32,34 @@ module.exports = class HandleController extends Controller {
                 account: [ 'nonempty' ],
                 password: [ 'nonempty' ],
             });
-            const data = await service.userService.curl('/api/v1/user/info', {
-                data: { email: account, password },
+            ctx.logger.info(`用户登录，查询是否有该用户：请求参数=> ${account}`);
+            let objUser = await service.userService.curl('/api/v1/user/info', {
+                data: { email: account },
             });
-            ctx.respSuccess(data);
+            if (!objUser) objUser = await service.userService.curl('/api/v1/user/info', {
+                data: { uid: account },
+            });
+            if (!objUser) {
+                ctx.logger.info(`用户登录，查询是否有该用户：返回结果=> 无该账号`);
+                throw '账号不存在';
+            }
+            let { _id, password: pwd, disabled, lock } = objUser;
+            if (disabled) {
+                ctx.logger.info(`用户登录，账号已禁用：用户账号=> ${account}`);
+                throw '账号已禁用';
+            }
+            if (disabled) {
+                ctx.logger.info(`用户登录，账号已锁定：用户账号=> ${account}`);
+                throw '账号已锁定';
+            }
+            if (password !== pwd) {
+                ctx.logger.info(`用户登录，密码错误：用户账号=> ${account}`);
+                throw '密码输入错误';
+            }
+            objUser.accessToken = await ctx.generateToken({ id: _id, user: objUser });
+            await ctx.kickOutUserById(_id);
+            delete objUser.password;
+            ctx.respSuccess(objUser);
         } catch (err) {
             ctx.respError(err);
         }
@@ -81,5 +105,6 @@ module.exports = class HandleController extends Controller {
             ctx.respError(err);
         }
     }
+
 
 };
--- gateway/front-end/app/service/UserService.js
+++ gateway/front-end/app/service/UserService.js
@@ -26,6 +26,57 @@ module.exports = class TransFormService extends CurlService {
     }
 
 
-    // 生成
+    // 验证账号
+    async auth ({ account, password, captcha }) {
+        
+        const { app, logger } = this;
+        const { redis } = app;
+        const { maxTimes, capTimes } = app.config.auth;
+        let objUser = await this.findOne({ nickname: account }, ' password');
+        if (!objUser)
+            objUser = await this.findOne({ phone: account }, ' password');
+        if (!objUser)
+            objUser = await this.findOne({ email: account }, ' password');
+        if (!objUser) {
+            logger.info(`账号:【${account}】不存在`);
+            throw '用户账号不存在或密码错误';
+        }
+        let { _id, password: pwd, disabled, lock } = objUser;
+        if (disabled) {
+            logger.info(`账号:【${account}】已禁用`);
+            throw 'F40005';
+        }
+        if (lock) {
+            logger.info(`账号:【${account}】已锁定`);
+            throw 'F40006';
+        }
+        let times = await redis.get(`${_id} auth password times`) || 0;
+        times = +times;
+        if (password !== pwd) {
+            times++;
+            if (times >= maxTimes) {
+                await this.unlock({ id: _id, lock: true }, true);
+                logger.info(`账号:【${account}】密码输入超过最大错误次数:【${maxTimes}】已锁定`);
+                await redis.del(`${_id} auth password times`);
+                throw 'F40006';
+            }
+            await redis.set(`${_id} auth password times`, times);
+            logger.info(`账号:【${account}】密码输入错误 还有${maxTimes - times}次机会`);
+            throw `密码输入错误，您还有${maxTimes - times}次机会`;
+        }
+        if (times) await redis.del(`${_id} auth password times`);
+        delete objUser.password;
+        return objUser;
+    }
+
+    // 生成 token
+    async token (objUser) {
+        const { ctx, app, config } = this;
+        const { _id } = objUser;
+        const { accessToken } = await ctx.generateToken({ id: _id, user: objUser });
+        objUser.access_token = accessToken;
+        await ctx.kickOutUserById(_id);
+        return objUser;
+    }
 
 };
