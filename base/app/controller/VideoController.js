dbb51834c172f55f7dd9a65a1387ed399dbd0dfc
Woshiajuana <zhigang.chen@owulia.com>
2019-12-04 17:42:39 +0800

    subcode

--- base/app/controller/VideoController.js
+++ base/app/controller/VideoController.js
@@ -10,6 +10,7 @@ module.exports = class HandleController extends Controller {
             .mount('/api/v1/video/update', controller.update)
             .mount('/api/v1/video/info', controller.info)
             .mount('/api/v1/video/list', controller.list)
+            .mount('/api/v1/video/del', controller.del)
         ;
     }
 
@@ -18,6 +19,11 @@ module.exports = class HandleController extends Controller {
      * @api {get} /api/v1/video/create 创建视频
      * @apiDescription 创建视频
      * @apiGroup 视频
+     * @apiParam  {String} [user] 用户 id
+     * @apiParam  {String} [video] 视频文件 id
+     * @apiParam  {String} [cover] 视频封面文件 id
+     * @apiParam  {String} [title] 视频标题 id
+     * @apiParam  {String} [nature] 视频性质
      * @apiSuccess (成功) {Object} data
      * @apiSampleRequest /api/v1/video/create
      */
@@ -46,11 +52,12 @@ module.exports = class HandleController extends Controller {
      * @apiDescription 更新视频
      * @apiGroup 视频
      * @apiParam  {String} [id] 视频 id
-     * @apiParam  {String} [nickname] 昵称
-     * @apiParam  {String} [password] 密码
-     * @apiParam  {String} [avatar] 头像
-     * @apiParam  {String} [sex] 性别
-     * @apiParam  {String} [signature] 个性签名
+     * @apiParam  {String} [user] 用户 id
+     * @apiParam  {String} [video] 视频文件 id
+     * @apiParam  {String} [cover] 视频封面文件 id
+     * @apiParam  {String} [title] 视频标题 id
+     * @apiParam  {String} [nature] 视频性质
+     * @apiParam  {String} [volume] 播放量
      * @apiSuccess (成功) {Object} data
      * @apiSampleRequest /api/v1/video/update
      */
@@ -58,16 +65,17 @@ module.exports = class HandleController extends Controller {
         const { ctx, service, app } = this;
         try {
             let objParams = await ctx.validateBody({
-                id: [],
-                nickname: [],
-                password: [],
-                avatar: [],
-                sex: [],
-                signature: [],
-                disabled: [],
-                lock: [],
+                user: [ 'nonempty' ],
+                id: [ 'nonempty' ],
+                video: [],
+                cover: [],
+                title: [],
+                volume: [],
+                nature: [],
             });
-            await service.userService.update(objParams);
+            ctx.logger.info(`更新视频信息：请求参数=> ${JSON.stringify(objParams)} `);
+            await service.videoService.update(objParams);
+            ctx.logger.info(`更新视频信息：返回结果=> 成功 `);
             ctx.respSuccess();
         } catch (err) {
             ctx.respError(err);
@@ -76,25 +84,21 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/user/info  查询视频
+     * @api {get} /api/v1/video/info 查询视频
      * @apiDescription 更新视频
      * @apiGroup 视频
      * @apiParam  {String} [id] 视频 id
-     * @apiParam  {String} [email] 视频邮箱
-     * @apiParam  {String} [uid] 7S-ID
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/user/info
+     * @apiSampleRequest /api/v1/video/info
      */
     async info () {
         const { ctx, service, app } = this;
         try {
             let objParams = await ctx.validateBody({
-                id: [ ],
-                email: [ ],
-                uid: [ ],
+                id: [ 'nonempty' ],
             });
             ctx.logger.info(`查询视频信息：请求参数=> ${JSON.stringify(objParams)} `);
-            const data = await service.userService.findOne(objParams);
+            const data = await service.videoService.findById(objParams);
             ctx.logger.info(`查询视频信息：返回结果=> ${JSON.stringify(data)} `);
             ctx.respSuccess(data);
         } catch (err) {
@@ -104,14 +108,40 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/user/list 查询视频列表
+     * @api {get} /api/v1/video/del 删除视频
+     * @apiDescription 删除视频
+     * @apiGroup 视频
+     * @apiParam  {String} [id] 视频 id
+     * @apiParam  {String} [user] 用户
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/video/del
+     */
+    async del () {
+        const { ctx, service, app } = this;
+        try {
+            let objParams = await ctx.validateBody({
+                id: [ 'nonempty' ],
+                user: [ 'nonempty' ],
+            });
+            ctx.logger.info(`删除视频信息：请求参数=> ${JSON.stringify(objParams)} `);
+            const data = await service.videoService.del(objParams);
+            ctx.logger.info(`删除视频信息：返回结果=> 成功 `);
+            ctx.respSuccess(data);
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/video/list 查询视频列表
      * @apiDescription 查询视频列表
      * @apiGroup APP基础
      * @apiParam  {String} [numIndex] 页数
      * @apiParam  {String} [numSize] 大小
-     * @apiParam  {String} [keyword] 邮箱 / 昵称 / uid
+     * @apiParam  {String} [keyword] 标题 / 时间
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/user/list
+     * @apiSampleRequest /api/v1/video/list
      */
     async list () {
         const { ctx, service, app } = this;
@@ -119,9 +149,10 @@ module.exports = class HandleController extends Controller {
             const objParams = await ctx.validateBody({
                 numIndex: [ 'nonempty' ],
                 numSize: [ 'nonempty' ],
+                user: [],
                 keyword: [],
             });
-            const data = await service.userService.list(objParams);
+            const data = await service.videoService.list(objParams);
             ctx.respSuccess(data);
         } catch (err) {
             ctx.respError(err);
--- base/app/service/VideoService.js
+++ base/app/service/VideoService.js
@@ -5,50 +5,59 @@ const { Service } = require('egg');
 
 module.exports = class HandleServer extends Service {
 
+    // 总视频数量
+    async count () {
+
+    }
+
     // 创建
     async create (data) {
         const { ctx } = this;
-        await ctx.model.UserModel.create(data);
+        await ctx.model.VideoModel.create(data);
     }
 
     // 更新
     async update (data) {
         const { ctx, app } = this;
-        const { id } = data;
+        const { id, user } = data;
         delete data.id;
-        await ctx.model.UserModel.update({ _id: app.mongoose.Types.ObjectId(id) }, data);
+        await ctx.model.VideoModel.update({
+            _id: app.mongoose.Types.ObjectId(id),
+            user: app.mongoose.Types.ObjectId(user),
+        }, data);
     }
 
     // 根据 id 查询
-    async findOne (data) {
+    async findById (id) {
         const { ctx } = this;
-        let { id, email, uid } = data;
-        if (id) return await ctx.model.UserModel.findById(id).lean();
-        if (email) return await ctx.model.UserModel.findOne({ email }).lean();
-        if (uid) return await ctx.model.UserModel.findOne({ uid }).lean();
+        return await ctx.model.VideoModel.findById(id).lean();
     }
 
     // 删除
-    async del (id) {
+    async del ({ id, user }) {
         const { ctx, app } = this;
-        await ctx.model.UserModel.remove({ _id: app.mongoose.Types.ObjectId(id) });
+        await ctx.model.VideoModel.remove({
+            _id: app.mongoose.Types.ObjectId(id),
+            user: app.mongoose.Types.ObjectId(user),
+        });
     }
 
     // 列表
     async list (data) {
         const { ctx, app } = this;
-        let { numIndex, numSize, keyword } = data;
+        let { numIndex, numSize, keyword, user } = data;
         numIndex = +numIndex;
         numSize = +numSize;
         let filter = { $or: [] }; // 多字段匹配
+        if (user) {
+            filter.user = app.mongoose.Types.ObjectId(user);
+        }
         if (keyword) {
-            filter.$or.push({ email: { $regex: keyword, $options: '$i' } });
-            filter.$or.push({ uid: { $regex: keyword, $options: '$i' } });
-            filter.$or.push({ nickname: { $regex: keyword, $options: '$i' } });
+            filter.$or.push({ title: { $regex: keyword, $options: '$i' } });
         }
         if (!filter.$or.length) delete filter.$or;
-        const total = await ctx.model.UserModel.count(filter);
-        const list = await ctx.model.UserModel
+        const total = await ctx.model.VideoModel.count(filter);
+        const list = await ctx.model.VideoModel
             .find(filter)
             .sort('-created_at')
             .skip((numIndex - 1) * numSize)
