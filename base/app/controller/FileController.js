89dc24e91ccf8c83bc8e8d82490903c37f12c4b5
Woshiajuana <zhigang.chen@owulia.com>
2019-12-04 17:49:08 +0800

    subcode

---
+++ base/app/controller/FileController.js
@@ -0,0 +1,127 @@
+
+'use strict';
+
+const { Controller } = require('egg');
+
+module.exports = class HandleController extends Controller {
+
+    static route (app, middleware, controller) {
+        app.router.mount('/api/v1/file/create', controller.create)
+            .mount('/api/v1/file/info', controller.info)
+            .mount('/api/v1/file/list', controller.list)
+            .mount('/api/v1/file/del', controller.del)
+        ;
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/file/create 创建文件
+     * @apiDescription 创建文件
+     * @apiGroup 文件
+     * @apiParam  {String} [user] 用户 id
+     * @apiParam  {String} [file] 文件文件 id
+     * @apiParam  {String} [cover] 文件封面文件 id
+     * @apiParam  {String} [title] 文件标题 id
+     * @apiParam  {String} [nature] 文件性质
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/file/create
+     */
+    async create () {
+        const { ctx, service, app } = this;
+        try {
+            let objParams = await ctx.validateBody({
+                user: [ 'nonempty' ],
+                ip: [ 'nonempty' ],
+                type: [ 'nonempty' ],
+                path: [ 'nonempty' ],
+                base: [ 'nonempty' ],
+                filename: [ 'nonempty' ],
+                device: [ 'nonempty' ],
+            });
+            ctx.logger.info(`创建文件：请求参数=> ${JSON.stringify(objParams)} `);
+            await service.fileService.create(objParams);
+            ctx.logger.info(`创建文件：返回结果=> 成功`);
+            ctx.respSuccess();
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/file/info 查询文件
+     * @apiDescription 更新文件
+     * @apiGroup 文件
+     * @apiParam  {String} [id] 文件 id
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/file/info
+     */
+    async info () {
+        const { ctx, service, app } = this;
+        try {
+            let objParams = await ctx.validateBody({
+                id: [ 'nonempty' ],
+            });
+            ctx.logger.info(`查询文件信息：请求参数=> ${JSON.stringify(objParams)} `);
+            const data = await service.fileService.findById(objParams);
+            ctx.logger.info(`查询文件信息：返回结果=> ${JSON.stringify(data)} `);
+            ctx.respSuccess(data);
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/file/del 删除文件
+     * @apiDescription 删除文件
+     * @apiGroup 文件
+     * @apiParam  {String} [id] 文件 id
+     * @apiParam  {String} [user] 用户
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/file/del
+     */
+    async del () {
+        const { ctx, service, app } = this;
+        try {
+            let objParams = await ctx.validateBody({
+                id: [ 'nonempty' ],
+                user: [ 'nonempty' ],
+            });
+            ctx.logger.info(`删除文件信息：请求参数=> ${JSON.stringify(objParams)} `);
+            const data = await service.fileService.del(objParams);
+            ctx.logger.info(`删除文件信息：返回结果=> 成功 `);
+            ctx.respSuccess(data);
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/file/list 查询文件列表
+     * @apiDescription 查询文件列表
+     * @apiGroup APP基础
+     * @apiParam  {String} [numIndex] 页数
+     * @apiParam  {String} [numSize] 大小
+     * @apiParam  {String} [keyword] 标题 / 时间
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/file/list
+     */
+    async list () {
+        const { ctx, service, app } = this;
+        try {
+            const objParams = await ctx.validateBody({
+                numIndex: [ 'nonempty' ],
+                numSize: [ 'nonempty' ],
+                user: [],
+                keyword: [],
+            });
+            const data = await service.fileService.list(objParams);
+            ctx.respSuccess(data);
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+};
