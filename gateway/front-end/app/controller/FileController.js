a5c20b1da0f67016638c58c23f94f36c8207c70f
Woshiajuana <zhigang.chen@owulia.com>
2019-12-05 14:09:14 +0800

    subcode

--- gateway/front-end/app/controller/FileController.js
+++ gateway/front-end/app/controller/FileController.js
@@ -6,14 +6,15 @@ const { Controller } = require('egg');
 module.exports = class HandleController extends Controller {
 
     static route (app, middleware, controller) {
-        app.router.mount('/api/v1/app/file/upload', middleware.tokenMiddleware(), controller.upload)
+        app.router.mount('/api/v1/app/file/upload-video', middleware.tokenMiddleware(), controller.uploadVideo)
+            .mount('/api/v1/app/file/upload-image', middleware.tokenMiddleware(), controller.uploadImage)
         ;
 
     }
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/app/file/upload 上传文件
+     * @api {get} /api/app/file/upload 上传视频文件
      * @apiDescription  File 文件模块
      * @apiGroup  文件
      * @apiParam  {String} [account] 账号
@@ -21,23 +22,51 @@ module.exports = class HandleController extends Controller {
      * @apiSuccess (成功) {Object} data
      * @apiSampleRequest /api/app/file/upload
      */
-    async upload () {
+    async uploadVideo () {
         const { ctx, service, app } = this;
         try {
             let {
-                account,
-                password,
+                file,
                 captcha,
             } = await ctx.validateBody({
                 account: [ 'nonempty' ],
                 password: [ 'nonempty' ],
                 captcha: [ ],
             });
-
             ctx.respSuccess();
         } catch (err) {
             ctx.respError(err);
         }
     }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/app/file/upload 上传视频文件
+     * @apiDescription  File 文件模块
+     * @apiGroup  文件
+     * @apiParam  {String} [account] 账号
+     * @apiParam  {String} [password] 密码
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/app/file/upload
+     */
+    async uploadImage () {
+        const { ctx, service, app } = this;
+        try {
+            let {
+                file,
+                captcha,
+            } = await ctx.validateBody({
+                account: [ 'nonempty' ],
+                password: [ 'nonempty' ],
+                captcha: [ ],
+            });
+            ctx.respSuccess();
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+
+
 
 };
