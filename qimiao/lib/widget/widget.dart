806d20f0c1063e0ec407cb0916995ba6c2141666
Woshiajuana <zhigang.chen@owulia.com>
2019-12-06 11:29:49 +0800

    subcode

--- qimiao/lib/widget/widget.dart
+++ qimiao/lib/widget/widget.dart
@@ -5,7 +5,6 @@ export 'confirmDialog.widget.dart';
 export 'upgradeDialog.widget.dart';
 export 'actionSheetDialog.widget.dart';
 export 'sliverCustomHeaderDelegate.widget.dart';
-export 'stickyTabBarDelegate.widget.dart';
 export 'stickyWidgetDelegate.widget.dart';
 export 'wowCalendar.widget.dart';
 export 'alertToastDialog.widget.dart';
