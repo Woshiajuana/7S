4d141239ab290d24109e2b637b9822a3ef1d5e3d
Woshiajuana <zhigang.chen@owulia.com>
2019-12-07 21:14:11 +0800

    subcode

--- gateway/front-end/app/controller/PhotoController.js
+++ gateway/front-end/app/controller/PhotoController.js
@@ -9,6 +9,7 @@ module.exports = class HandleController extends Controller {
         app.router.mount('/api/v1/app/photo/list', middleware.tokenMiddleware(), controller.list)
             .mount('/api/v1/app/photo/info', middleware.tokenMiddleware(), controller.info)
             .mount('/api/v1/app/photo/del', middleware.tokenMiddleware(), controller.del)
+            .mount('/api/v1/app/photo/update', middleware.tokenMiddleware(), controller.update)
         ;
     }
 
@@ -82,5 +83,26 @@ module.exports = class HandleController extends Controller {
         }
     }
 
-
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/app/photo/update 照片更新
+     * @apiDescription  照片模块
+     * @apiGroup  文件
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/app/photo/update
+     */
+    async update () {
+        const { ctx, service, app } = this;
+        try {
+            let {
+                type,
+            } = await ctx.validateBody({
+                // 类型 [ AVATAR: 头像, VIDEO: 视频,  PHOTO: 照片, COVER: 封面 ]
+                type: [ 'nonempty', (v) => ['AVATAR', 'VIDEO', 'PHOTO', 'COVER'].indexOf(v) > -1 ],
+            });
+            ctx.respSuccess();
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
 };
