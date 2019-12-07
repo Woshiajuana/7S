44d946e13197c9c43967a80466612cf0d71e3567
Woshiajuana <zhigang.chen@owulia.com>
2019-12-07 21:05:46 +0800

    subcode

--- gateway/front-end/app/controller/NoticeController.js
+++ gateway/front-end/app/controller/NoticeController.js
@@ -14,69 +14,21 @@ module.exports = class HandleController extends Controller {
     /**
      * @apiVersion 1.0.0
      * @api {get} /api/v1/app/notice/list 上传文件
-     * @apiDescription  File 文件模块
+     * @apiDescription  通讯消息模块
      * @apiGroup  文件
-     * @apiParam  {String} [account] 账号
-     * @apiParam  {String} [password] 密码
      * @apiSuccess (成功) {Object} data
      * @apiSampleRequest /api/v1/app/notice/list
      */
     async list () {
         const { ctx, service, app } = this;
         try {
-            let [
-                file,
-            ] = await ctx.validateFiles([
-                [ 'nonempty' ]
-            ]);
-            let {
-                ip,
-                userAgent = {},
-            } = ctx;
-            let {
-                bucket,
-                endpoint,
-                rootDir,
-            } = app.config.oss.client;
-            let {
-                id,
-            } = ctx.state.token;
             let {
                 type,
             } = await ctx.validateBody({
                 // 类型 [ AVATAR: 头像, VIDEO: 视频,  PHOTO: 照片, COVER: 封面 ]
                 type: [ 'nonempty', (v) => ['AVATAR', 'VIDEO', 'PHOTO', 'COVER'].indexOf(v) > -1 ],
             });
-            let result;
-            let { filepath, filename } = file;
-            let strPath = `${rootDir}/${id}/${type}/`;
-            let strName = `${moment().format('YYYYMMDDHHmmss')}.${filename.split('.')[1]}`;
-            try {
-                result = await ctx.oss.put(`${strPath}${strName}`, filepath);
-            } catch (e) {
-                this.logger.info(`上传OSS失败=> ${result.toString()}`);
-            } finally {
-                await fs.unlink(filepath);
-            }
-            if (!result) throw '文件上传失败';
-            const {
-                _id
-            } = await service.transformService.curl('api/v1/file/create', {
-                data: {
-                    user: id,
-                    type,
-                    ip,
-                    base: `https://${bucket}.${endpoint}/`,
-                    path: strPath,
-                    filename: strName,
-                    device: JSON.stringify(userAgent),
-                    source: filename,
-                },
-            });
-            ctx.respSuccess({
-                url: `https://${bucket}.${endpoint}/${strPath}${strName}`,
-                file: _id,
-            });
+            ctx.respSuccess();
         } catch (err) {
             ctx.respError(err);
         }
--- gateway/front-end/app/controller/VideoController.js
+++ gateway/front-end/app/controller/VideoController.js
@@ -6,77 +6,29 @@ const { Controller } = require('egg');
 module.exports = class HandleController extends Controller {
 
     static route (app, middleware, controller) {
-        app.router.mount('/api/v1/app/video/upload', middleware.tokenMiddleware(), controller.upload)
+        app.router.mount('/api/v1/app/video/upload', middleware.tokenMiddleware(), controller.list)
         ;
     }
 
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/app/file/upload 上传文件
-     * @apiDescription  File 文件模块
+     * @api {get} /api/v1/app/notice/list 上传文件
+     * @apiDescription  通讯消息模块
      * @apiGroup  文件
-     * @apiParam  {String} [account] 账号
-     * @apiParam  {String} [password] 密码
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/app/file/upload
+     * @apiSampleRequest /api/v1/app/notice/list
      */
-    async upload () {
+    async list () {
         const { ctx, service, app } = this;
         try {
-            let [
-                file,
-            ] = await ctx.validateFiles([
-                [ 'nonempty' ]
-            ]);
-            let {
-                ip,
-                userAgent = {},
-            } = ctx;
-            let {
-                bucket,
-                endpoint,
-                rootDir,
-            } = app.config.oss.client;
-            let {
-                id,
-            } = ctx.state.token;
             let {
                 type,
             } = await ctx.validateBody({
                 // 类型 [ AVATAR: 头像, VIDEO: 视频,  PHOTO: 照片, COVER: 封面 ]
                 type: [ 'nonempty', (v) => ['AVATAR', 'VIDEO', 'PHOTO', 'COVER'].indexOf(v) > -1 ],
             });
-            let result;
-            let { filepath, filename } = file;
-            let strPath = `${rootDir}/${id}/${type}/`;
-            let strName = `${moment().format('YYYYMMDDHHmmss')}.${filename.split('.')[1]}`;
-            try {
-                result = await ctx.oss.put(`${strPath}${strName}`, filepath);
-            } catch (e) {
-                this.logger.info(`上传OSS失败=> ${result.toString()}`);
-            } finally {
-                await fs.unlink(filepath);
-            }
-            if (!result) throw '文件上传失败';
-            const {
-                _id
-            } = await service.transformService.curl('api/v1/file/create', {
-                data: {
-                    user: id,
-                    type,
-                    ip,
-                    base: `https://${bucket}.${endpoint}/`,
-                    path: strPath,
-                    filename: strName,
-                    device: JSON.stringify(userAgent),
-                    source: filename,
-                },
-            });
-            ctx.respSuccess({
-                url: `https://${bucket}.${endpoint}/${strPath}${strName}`,
-                file: _id,
-            });
+            ctx.respSuccess();
         } catch (err) {
             ctx.respError(err);
         }
