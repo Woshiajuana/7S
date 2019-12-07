33563c2b1d3c54f75e1872cb038ff4eca9637f03
Woshiajuana <zhigang.chen@owulia.com>
2019-12-07 21:10:32 +0800

    subcode

--- gateway/front-end/app/controller/NoticeController.js
+++ gateway/front-end/app/controller/NoticeController.js
@@ -7,19 +7,43 @@ module.exports = class HandleController extends Controller {
 
     static route (app, middleware, controller) {
         app.router.mount('/api/v1/app/notice/list', middleware.tokenMiddleware(), controller.list)
+            .mount('/api/v1/app/notice/info', middleware.tokenMiddleware(), controller.info)
         ;
     }
 
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/notice/list 上传文件
+     * @api {get} /api/v1/app/notice/list 消息列表
      * @apiDescription  通讯消息模块
      * @apiGroup  文件
      * @apiSuccess (成功) {Object} data
      * @apiSampleRequest /api/v1/app/notice/list
      */
     async list () {
+        const { ctx, service, app } = this;
+        try {
+            let {
+                type,
+            } = await ctx.validateBody({
+                // 类型 [ AVATAR: 头像, VIDEO: 视频,  PHOTO: 照片, COVER: 封面 ]
+                type: [ 'nonempty', (v) => ['AVATAR', 'VIDEO', 'PHOTO', 'COVER'].indexOf(v) > -1 ],
+            });
+            ctx.respSuccess();
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/app/notice/info 消息详情
+     * @apiDescription  通讯消息模块
+     * @apiGroup  文件
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/app/notice/info
+     */
+    async info () {
         const { ctx, service, app } = this;
         try {
             let {
