73acc245159e3f34cea9e39855aeb0f8a4e5b79b
Woshiajuana <zhigang.chen@owulia.com>
2019-12-13 14:01:12 +0800

    subcode code

--- qimiao/lib/common/config/env.config.dart
+++ qimiao/lib/common/config/env.config.dart
@@ -11,7 +11,8 @@ class Env {
   Env._internal();
 
   String get baseUrl {
-    return 'http://192.168.3.195:9002/api/';
+//    return 'http://192.168.3.195:9002/api/';
+    return 'http://154.8.209.13:9002/api/';
   }
 
   List<String> get arrSucCode {
