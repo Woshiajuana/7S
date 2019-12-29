fe074bc46b7c911c391a1a694d46ccf5f8da6bff
Woshiajuana <zhigang.chen@owulia.com>
2019-12-29 21:10:11 +0800

    subcode code

---
+++ gateway/front-end/app/controller/CollectController.js
@@ -0,0 +1,70 @@
+
+'use strict';
+
+const { Controller } = require('egg');
+
+module.exports = class HandleController extends Controller {
+
+    static route (app, middleware, controller) {
+        app.router.mount('/api/v1/app/collect/create', middleware.tokenMiddleware(), controller.create)
+            .mount('/api/v1/app/collect/list', middleware.tokenMiddleware(), controller.list)
+            .mount('/api/v1/app/collect/del', middleware.tokenMiddleware(), controller.del)
+        ;
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/collect/create 创建收藏
+     * @apiDescription 创建收藏
+     * @apiGroup 收藏
+     * @apiParam  {String} [user] 用户 id
+     * @apiParam  {String} [photo] 照片
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/collect/create
+     */
+    async create () {
+        const { ctx, service, app } = this;
+        try {
+            let objParams = await ctx.validateBody({
+                user: [ 'nonempty' ],
+                photo: [ 'nonempty' ],
+            });
+            ctx.logger.info(`创建收藏：请求参数=> ${JSON.stringify(objParams)} `);
+            await service.collectService.create(objParams);
+            ctx.logger.info(`创建收藏：返回结果=> 成功`);
+            ctx.respSuccess();
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/app/collect/list 查询收藏
+     * @apiDescription 更新收藏
+     * @apiGroup 收藏
+     * @apiParam  {String} [numIndex] 页数
+     * @apiParam  {String} [numSize] 大小
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/app/collect/list
+     */
+    async list () {
+        const { ctx, service, app } = this;
+        try {
+            const objParams = await ctx.validateBody({
+                numIndex: [ 'nonempty' ],
+                numSize: [ 'nonempty' ],
+            });
+            const { id } = ctx.state.token;
+            const data = await service.transformService.curl('api/v1/history/list', {
+                data: { ...objParams, user: id },
+            });
+            ctx.respSuccess(data);
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+
+
+};
