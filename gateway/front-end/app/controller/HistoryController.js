9a76d0556e0ac68fb16586ef845e80c2d254ceb9
Woshiajuana <zhigang.chen@owulia.com>
2019-12-29 21:02:51 +0800

    subcode code

--- gateway/front-end/app/controller/HistoryController.js
+++ gateway/front-end/app/controller/HistoryController.js
@@ -7,6 +7,7 @@ module.exports = class HandleController extends Controller {
 
     static route (app, middleware, controller) {
         app.router.mount('/api/v1/app/history/list', middleware.tokenMiddleware(), controller.list)
+            .mount('/api/v1/app/history/del', middleware.tokenMiddleware(), controller.del)
         ;
     }
 
@@ -17,8 +18,6 @@ module.exports = class HandleController extends Controller {
      * @apiGroup APP基础
      * @apiParam  {String} [numIndex] 页数
      * @apiParam  {String} [numSize] 大小
-     * @apiParam  {String} [user] 用户 id
-     * @apiParam  {String} [photo] 照片
      * @apiSuccess (成功) {Object} data
      * @apiSampleRequest /api/v1/app/history/list
      */
@@ -32,6 +31,31 @@ module.exports = class HandleController extends Controller {
             const { id } = ctx.state.token;
             const data = await service.transformService.curl('api/v1/history/list', {
                 data: { ...objParams, user: id },
+            });
+            ctx.respSuccess(data);
+        } catch (err) {
+            ctx.respError(err);
+        }
+    }
+
+    /**
+     * @apiVersion 1.0.0
+     * @api {get} /api/v1/app/history/del 删除观看历史列表
+     * @apiDescription 删除观看历史列表
+     * @apiGroup APP基础
+     * @apiParam  {String} [id] id
+     * @apiSuccess (成功) {Object} data
+     * @apiSampleRequest /api/v1/app/history/del
+     */
+    async del () {
+        const { ctx, service, app } = this;
+        try {
+            const objParams = await ctx.validateBody({
+                id: [ 'nonempty' ],
+            });
+            const { id: user } = ctx.state.token;
+            const data = await service.transformService.curl('api/v1/history/del', {
+                data: { ...objParams, user },
             });
             ctx.respSuccess(data);
         } catch (err) {
