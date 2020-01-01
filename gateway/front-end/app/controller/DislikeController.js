20354cf1dbc6fcb1c14e107f466056d3c69ad266
Woshiajuana <zhigang.chen@owulia.com>
2020-01-01 21:38:14 +0800

    subcode

--- gateway/front-end/app/controller/DislikeController.js
+++ gateway/front-end/app/controller/DislikeController.js
@@ -6,23 +6,21 @@ const { Controller } = require('egg');
 module.exports = class HandleController extends Controller {
 
     static route (app, middleware, controller) {
-        app.router.mount('/api/v1/app/dislike/create', middleware.tokenMiddleware(), controller.create)
+        app.router.mount('/api/v1/app/dislike/do', middleware.tokenMiddleware(), controller.create)
             .mount('/api/v1/app/dislike/list', middleware.tokenMiddleware(), controller.list)
-            .mount('/api/v1/app/dislike/del', middleware.tokenMiddleware(), controller.del)
         ;
     }
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/dislike/create 创建不喜欢
-     * @apiDescription 创建不喜欢
+     * @api {get} /api/v1/dislike/do 不喜欢 or 取消
+     * @apiDescription 不喜欢 or 取消
      * @apiGroup 不喜欢
-     * @apiParam  {String} [user] 用户 id
      * @apiParam  {String} [photo] 照片
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/dislike/create
+     * @apiSampleRequest /api/v1/dislike/do
      */
-    async create () {
+    async do () {
         const { ctx, service, app } = this;
         try {
             let objParams = await ctx.validateBody({
@@ -30,6 +28,22 @@ module.exports = class HandleController extends Controller {
                 photo: [ 'nonempty' ],
             });
             ctx.logger.info(`创建不喜欢：请求参数=> ${JSON.stringify(objParams)} `);
+            const { id: user } = ctx.state.token;
+            let data = await service.transformService.curl('api/v1/dislike/info', {
+                data: { ...objParams, user },
+            });
+            if (data) {
+                // 取消
+                await service.transformService.curl('api/v1/dislike/del', {
+                    data: { ...objParams, user },
+                });
+                data = '';
+            } else {
+                // 创建
+                data = await service.transformService.curl('api/v1/dislike/create', {
+                    data: { ...objParams, user },
+                });
+            }
             ctx.logger.info(`创建不喜欢：返回结果=> 成功`);
             ctx.respSuccess();
         } catch (err) {
@@ -63,32 +77,5 @@ module.exports = class HandleController extends Controller {
             ctx.respError(err);
         }
     }
-
-    /**
-     * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/dislike/del 删除不喜欢
-     * @apiDescription 删除不喜欢
-     * @apiGroup 不喜欢
-     * @apiParam  {String} [id] id
-     * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/dislike/del
-     */
-    async del () {
-        const { ctx, service, app } = this;
-        try {
-            const objParams = await ctx.validateBody({
-                id: [ 'nonempty' ],
-            });
-            const { id } = ctx.state.token;
-            const data = await service.transformService.curl('api/v1/dislike/del', {
-                data: { ...objParams, user: id },
-            });
-            ctx.respSuccess(data);
-        } catch (err) {
-            ctx.respError(err);
-        }
-    }
-
-
-
+    
 };
