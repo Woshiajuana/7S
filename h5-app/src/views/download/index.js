5dd0ee73ba0cdee7230e6a52df3abe114be05c90
Woshiajuana <zhigang.chen@owulia.com>
2020-01-03 16:51:12 +0800

    subcode code

--- h5-app/src/views/download/index.js
+++ h5-app/src/views/download/index.js
@@ -1,3 +1,4 @@
+
 import DeviceUtil       from 'utils/device.util'
 import Toast            from 'utils/toast.util'
 
@@ -16,14 +17,11 @@ const DownloadController = {
         this.$elBtn.on('click', this.handleDown.bind(this));
     },
     handleDown () {
-        let url = ANDROID_URL;
+        if (this.device.isIphone)
+            return Toast.msg('很抱歉暂时还未发布苹果版本');
         if (this.isWeChat && this.isQq)
             return Toast.msg('请使用系统浏览器打开本页面进行下载');
-        if (this.device.isAndroid)
-            url = ANDROID_URL;
-        else if (this.device.isIphone)
-            url = iOS_URL;
-        window.location.href = url;
+        window.location.href = '';
     },
 };
 DownloadController.init();
