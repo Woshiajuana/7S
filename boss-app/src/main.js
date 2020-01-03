da3bc0abc1af3aa73f12aa2e68c2251f4c1c198f
Woshiajuana <zhigang.chen@owulia.com>
2020-01-03 16:04:11 +0800

    subcode code

---
+++ boss-app/src/main.js
@@ -0,0 +1,29 @@
+
+const views = require.context('./views', true, /index\.vue$/);
+const components = require.context('./components', true, /index\.vue$/);
+
+let { wow, app } = window.wowRuntime.init({
+    // 扩展类配置, 这个类里面的数据都会扩展挂载到 VUE 上
+    extendUtils: {
+        testUtils: () => { console.log('cacaca') },
+    },
+    // API配置
+    httpRequest: {
+        baseURL: `${window.location.protocol}//${window.location.hostname}:9003/`,
+        timeout: 3000,
+    },
+    // app 常量配置
+    appConst: {
+
+    },
+    // 路由配置
+    routerConfig: {
+        importViews: [ views ],
+    },
+    // 组件配置
+    component: {
+        importComponents: [ components ],
+    },
+});
+
+console.log(wow);
---
+++ boss-app/vue.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const path = require('path');
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
+
+// 具体配置参数 https://cli.vuejs.org/config/
+module.exports = {
+    publicPath: './',
+    // outputDir: 'dist',
+    // assetsDir: 'static',
+    lintOnSave: false,
+    productionSourceMap: false,
+    configureWebpack: {
+        name: 'Wow-Admin',
+        resolve: {
+            alias: {
+                '@': resolve('src'),
+                '@components': resolve('src/components'),
+                '@router': resolve('src/router'),
+                '@store': resolve('src/store'),
+                '@layout': resolve('src/layout'),
+                '@assets': resolve('src/assets'),
+                '@views': resolve('src/views'),
+                '@utils': resolve('src/utils'),
+            }
+        }
+    },
+    chainWebpack (config) {
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/assets/icons'))
+            .end();
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/assets/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end();
+    },
+};
