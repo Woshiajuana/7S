8e2f72c280bad04d498ab1b6d7776d873079e45d
Woshiajuana <zhigang.chen@owulia.com>
2020-01-04 20:10:16 +0800

    subcode

---
+++ boss-app/src/views/notice/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="wrap">
+        <filter-view
+            :filter-form="objFilterForm"
+            :filter-button="arrFilterButton"
+            @filter="reqTableDataList"
+        ></filter-view>
+        <span>业务页面</span>
+        <test-component></test-component>
+    </div>
+</template>
+
+<script>
+    import DialogMixin from '@/mixins/dialog'
+    import FilterMixin from '@/mixins/filter'
+    import DataMixin from './data.mixin'
+
+    export default {
+        name: 'AdminUser1',
+        mixins: [
+            DataMixin,
+            DialogMixin,
+            FilterMixin,
+        ],
+        created () {
+            this.$testUtils();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~@assets/scss/define.scss";
+    .inner{
+        @extend %bsb;
+        padding: 10px;
+    }
+</style>
