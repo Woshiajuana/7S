12918a451ef74b1f416b5b64baae227c00ac8496
Woshiajuana <zhigang.chen@owulia.com>
2020-03-05 16:38:25 +0800

    subcode code

--- gateway/miniprogram-end/app/controller/UserController.js
+++ gateway/miniprogram-end/app/controller/UserController.js
@@ -6,83 +6,95 @@ const { Controller } = require('egg');
 module.exports = class HandleController extends Controller {
 
     static route (app, middleware, controller) {
-        app.router.mount('/api/v1/app/user/login', controller.login)
-            .mount('/api/v1/app/user/register', controller.register)
-            .mount('/api/v1/app/user/info', middleware.tokenMiddleware(), controller.info)
-            .mount('/api/v1/app/user/update', middleware.tokenMiddleware(), controller.update)
-            .mount('/api/v1/app/user/change/password', middleware.tokenMiddleware(), controller.changePassword)
-            .mount('/api/v1/app/user/reset/password', controller.resetPassword)
-            .mount('/api/v1/app/user/reset/captcha', controller.resetCaptcha)
+        app.router.mount('/api/v1/wx/user/login', controller.login)
+            .mount('/api/v1/wx/user/register', controller.register)
+            .mount('/api/v1/wx/user/info', middleware.tokenMiddleware(), controller.info)
+            .mount('/api/v1/wx/user/update', middleware.tokenMiddleware(), controller.update)
+            .mount('/api/v1/wx/user/change/password', middleware.tokenMiddleware(), controller.changePassword)
+            .mount('/api/v1/wx/user/reset/password', controller.resetPassword)
+            .mount('/api/v1/wx/user/reset/captcha', controller.resetCaptcha)
         ;
 
     }
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/login 用户登录
+     * @api {get} /api/v1/wx/user/login 用户登录
      * @apiDescription  User 用户模块
      * @apiGroup 用户
      * @apiParam  {String} [account] 账号
      * @apiParam  {String} [password] 密码
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/login
+     * @apiSampleRequest /api/v1/wx/user/login
      */
     async login () {
         const { ctx, service, app } = this;
         try {
-            let {
-                redis,
-                config,
-            } = app;
-            let {
-                maxTimes,
-            } = config.auth;
-            let {
-                account,
-                password,
-                captcha,
-            } = await ctx.validateBody({
-                account: [ 'nonempty' ],
-                password: [ 'nonempty' ],
-                captcha: [ ],
+            let objParams = await ctx.validateBody({
+                code: [ 'nonempty' ],
+                iv: [ 'nonempty' ],
+                encryptedData: [ 'nonempty' ],
+                avatarUrl: [ 'nonempty' ],
+                city: [ 'nonempty' ],
+                country: [ 'nonempty' ],
+                gender: [ 'nonempty' ],
+                language: [ 'nonempty' ],
+                nickName: [ 'nonempty' ],
+                province: [ 'nonempty' ],
             });
-            // 首先判断密码验证次数
-            let numTimes = +(await redis.get(`${account} auth password times`) || 0);
-            if (numTimes >= maxTimes) {
-                if (!(await service.captchaService.validate(account, captcha))) {
-                    throw { code: 'F50001', data: await service.captchaService.generate(account), msg: '图形验证码错误' }
-                }
-                await redis.del(`${account} auth password times`);
-            }
-            ctx.logger.info(`用户登录，查询是否有该用户：请求参数=> ${account}`);
-            let objUser = await service.transformService.curl('api/v1/user/one', {
-                data: { email: account },
-            });
-            if (!objUser) {
-                ctx.logger.info(`用户登录，查询是否有该用户：返回结果=> 无该账号`);
-                throw '账号不存在';
-            }
-            let { _id, password: pwd, disabled, lock } = objUser;
-            if (disabled) {
-                ctx.logger.info(`用户登录，账号已禁用：用户账号=> ${account}`);
-                throw '账号已禁用';
-            }
-            if (lock) {
-                ctx.logger.info(`用户登录，账号已锁定：用户账号=> ${account}`);
-                throw '账号已锁定';
-            }
-            if (password !== pwd) {
-                ctx.logger.info(`用户登录，密码错误：用户账号=> ${account}`);
-                await redis.set(`${account} auth password times`, ++numTimes);
-                throw numTimes >= maxTimes
-                    ? { code: 'F50001', data: await service.captchaService.generate(account), msg: captcha ? '密码输入错误' : '密码错误次数过多，请输入图形验证码' }
-                    : '密码输入错误';
-            }
-            delete objUser.password;
-            const { accessToken } = await ctx.generateToken({ id: _id, user: objUser });
-            objUser.accessToken = accessToken;
-            await ctx.kickOutUserById(_id);
-            ctx.respSuccess(objUser);
+            // let {
+            //     redis,
+            //     config,
+            // } = app;
+            // let {
+            //     maxTimes,
+            // } = config.auth;
+            // let {
+            //     account,
+            //     password,
+            //     captcha,
+            // } = await ctx.validateBody({
+            //     account: [ 'nonempty' ],
+            //     password: [ 'nonempty' ],
+            //     captcha: [ ],
+            // });
+            // // 首先判断密码验证次数
+            // let numTimes = +(await redis.get(`${account} auth password times`) || 0);
+            // if (numTimes >= maxTimes) {
+            //     if (!(await service.captchaService.validate(account, captcha))) {
+            //         throw { code: 'F50001', data: await service.captchaService.generate(account), msg: '图形验证码错误' }
+            //     }
+            //     await redis.del(`${account} auth password times`);
+            // }
+            // ctx.logger.info(`用户登录，查询是否有该用户：请求参数=> ${account}`);
+            // let objUser = await service.transformService.curl('api/v1/user/one', {
+            //     data: { email: account },
+            // });
+            // if (!objUser) {
+            //     ctx.logger.info(`用户登录，查询是否有该用户：返回结果=> 无该账号`);
+            //     throw '账号不存在';
+            // }
+            // let { _id, password: pwd, disabled, lock } = objUser;
+            // if (disabled) {
+            //     ctx.logger.info(`用户登录，账号已禁用：用户账号=> ${account}`);
+            //     throw '账号已禁用';
+            // }
+            // if (lock) {
+            //     ctx.logger.info(`用户登录，账号已锁定：用户账号=> ${account}`);
+            //     throw '账号已锁定';
+            // }
+            // if (password !== pwd) {
+            //     ctx.logger.info(`用户登录，密码错误：用户账号=> ${account}`);
+            //     await redis.set(`${account} auth password times`, ++numTimes);
+            //     throw numTimes >= maxTimes
+            //         ? { code: 'F50001', data: await service.captchaService.generate(account), msg: captcha ? '密码输入错误' : '密码错误次数过多，请输入图形验证码' }
+            //         : '密码输入错误';
+            // }
+            // delete objUser.password;
+            // const { accessToken } = await ctx.generateToken({ id: _id, user: objUser });
+            // objUser.accessToken = accessToken;
+            // await ctx.kickOutUserById(_id);
+            ctx.respSuccess(objParams);
         } catch (err) {
             ctx.respError(err);
         }
@@ -90,14 +102,14 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/register 用户注册
+     * @api {get} /api/v1/wx/user/register 用户注册
      * @apiDescription  User 用户模块
      * @apiGroup 用户
      * @apiParam  {String} [email] 账号
      * @apiParam  {String} [password] 密码
      * @apiParam  {String} [captcha] 验证码
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/register
+     * @apiSampleRequest /api/v1/wx/user/register
      * */
     async register () {
         const { ctx, service } = this;
@@ -134,11 +146,11 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/info 用户信息
+     * @api {get} /api/v1/wx/user/info 用户信息
      * @apiDescription  User 用户信息
      * @apiGroup 用户
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/info
+     * @apiSampleRequest /api/v1/wx/user/info
      * */
     async info () {
         const { ctx, service } = this;
@@ -171,7 +183,7 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/update 用户修改信息
+     * @api {get} /api/v1/wx/user/update 用户修改信息
      * @apiDescription  User 用户模块
      * @apiGroup 用户
      * @apiParam  {String} [nickname] nickname
@@ -179,7 +191,7 @@ module.exports = class HandleController extends Controller {
      * @apiParam  {String} [sex] sex
      * @apiParam  {String} [signature] signature
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/update
+     * @apiSampleRequest /api/v1/wx/user/update
      * */
     async update () {
         const { ctx, service } = this;
@@ -204,13 +216,13 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/change/password 用户修改密码
+     * @api {get} /api/v1/wx/user/change/password 用户修改密码
      * @apiDescription  User 用户模块
      * @apiGroup 用户
      * @apiParam  {String} [password]  新密码
      * @apiParam  {String} [oldPassword] 旧密码
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/change/password
+     * @apiSampleRequest /api/v1/wx/user/change/password
      * */
     async changePassword () {
         const { ctx, service } = this;
@@ -239,14 +251,14 @@ module.exports = class HandleController extends Controller {
 
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/reset/password 用户重置密码
+     * @api {get} /api/v1/wx/user/reset/password 用户重置密码
      * @apiDescription  User 用户模块
      * @apiGroup 用户
      * @apiParam  {String} [password] 密码
      * @apiParam  {String} [captcha] 验证码
      * @apiParam  {String} [email]  邮箱
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/reset/password
+     * @apiSampleRequest /api/v1/wx/user/reset/password
      * */
     async resetPassword () {
         const { ctx, service } = this;
@@ -271,12 +283,12 @@ module.exports = class HandleController extends Controller {
      * */
     /**
      * @apiVersion 1.0.0
-     * @api {get} /api/v1/app/user/reset/captcha 用户重置图形验证码
+     * @api {get} /api/v1/wx/user/reset/captcha 用户重置图形验证码
      * @apiDescription  User 用户模块
      * @apiGroup 用户
      * @apiParam  {String} [account]  邮箱账号
      * @apiSuccess (成功) {Object} data
-     * @apiSampleRequest /api/v1/app/user/reset/captcha
+     * @apiSampleRequest /api/v1/wx/user/reset/captcha
      * */
     async resetCaptcha () {
         const { ctx, service } = this;
